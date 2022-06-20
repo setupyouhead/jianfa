bd8316043fc934ad654135337f753a9aa8ec2792
liuwenchao <liuwenchao@atguigu.com>
2022-06-20 22:17:40 +0800

    create bixiejianfa

--- src/main/java/com/atguigu/git/wg/BiXieJianFa.java
+++ src/main/java/com/atguigu/git/wg/BiXieJianFa.java
@@ -10,7 +10,7 @@ public class BiXieJianFa {
     public static void main(String[] args) {
         System.out.println("byjf");
         System.out.println("byjf");
-        System.out.println("byjf");
+        System.out.println("byjfi");
         System.out.println("byjf");
     }
 }
